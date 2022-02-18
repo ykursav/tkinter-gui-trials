fbe64e3b2be4f1a465b3e674f0b3008ef8bf5cf6
ykursav <emrekursav91@hotmail.com>
2022-02-18 14:53:57 +0100

    Border types added.

--- frame_examples/frame_borders.py
+++ frame_examples/frame_borders.py
@@ -1,4 +1,34 @@
 import tkinter as tk
 
-BORDER_TYPES = {"flat_b": tk.FLAT,
+window_frames = tk.Tk()
+
+BORDER_TYPES = {
+    "sunken": tk.SUNKEN,
+    "no_border": tk.FLAT,
+    "raised": tk.RAISED,
+    "groove": tk.GROOVE,
+    "ridge": tk.RIDGE,
 }
+
+for border_name, border_type in BORDER_TYPES.items():
+    frame_loop = tk.Frame(
+        master=window_frames,
+        relief=border_type,
+        borderwidth=4,
+        padx=10,
+        pady=10,
+        bg="white",
+    )
+    frame_loop.pack(side=tk.RIGHT)
+    button = tk.Button(
+        master=frame_loop,
+        text=border_name,
+        activebackground="yellow",
+        background="red",
+        padx=10,
+        pady=10,
+    )
+    button.pack(side=tk.TOP)
+
+
+window_frames.mainloop()
